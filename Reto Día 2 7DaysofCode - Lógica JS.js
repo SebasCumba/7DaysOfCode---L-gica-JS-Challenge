f22644688e1294242271b1f385658170226e5cd2
SebasCumba <juanlopezcumba@outlook.com>
2025-02-17 21:21:10 -0500

    Reto Día 2 7DaysofCode - Lógica JS
    Well, here goes the second day, this one cost me a little more than the previous one, but for now this is my answer to this exercise. ##7DaysOfCode

---
+++ Reto Día 2 7DaysofCode - Lógica JS.js
@@ -0,0 +1,19 @@
+// Reto Día 2 7DaysofCode - Lógica JS
+/* 1. Reformulación del Porgrama: "Realizar una encuesta donde preguntes nombre, edad y que lenguaje practicas
+y al final mostrar un mensaje + pregunta opcional" */
+/* 2. Resultados Esperados: "Un cuadro interactivo donde aparezcan las preguntas" */
+
+let userName = prompt("¿Cuál es tu nombre?")
+let userAge = Number(prompt("¿Cuántos años tienes?"))
+let userLanguage = prompt("¿Qué lenguaje de programación estás estudiando?")
+
+alert (`¡Hola! ${userName}, tienes ${userAge} y ya estás aprendiendo ${userLanguage}`)
+
+let userAnswer = Number(prompt (`¿Te gusta estudiar ${userLanguage}? Responde con el número 1 para SÍ o 2 para NO`))
+
+if (userAnswer == 1) {
+    alert ("¡Muy bien! Sigue estudiando y tendrás mucho éxito.")
+    
+} if (userAnswer == 2) {
+    alert ("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?")
+}
